10880cb8f8c6ed0d11c5edf681518eb534e1b0b8
James1086 <james.castelow@gmail.com>
2017-08-26 23:32:54 +0100

    no message

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-	console.log(socket.sessionid);
+	console.log(socket);
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
