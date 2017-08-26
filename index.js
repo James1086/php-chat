0c3f6b372e494317409bbe72301147a586492fc5
James1086 <james.castelow@gmail.com>
2017-08-26 23:27:04 +0100

    no message

--- index.js
+++ index.js
@@ -7,51 +7,13 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+io.on('connection', function(socket){
+	console.log(this.socket.sessionid);
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
-let players = [];
-let current_turn = 0;
-let timeOut;
-let _turn = 0;
-const MAX_WAITING = 5000;
-
-function next_turn(){
-  _turn = current_turn++ % players.length;
-  players[_turn].emit('your_turn');
-  console.log("next turn triggered " , _turn);
-  triggerTimeout();
-}
-
-function triggerTimeout(){
- timeOut = setTimeout(()=>{
-   next_turn();
- },MAX_WAITING);
-}
-
-function resetTimeOut(){
-  if(typeof timeOut === 'object'){
-	console.log("timeout reset");
-	clearTimeout(timeOut);
-  }
-}
-
- io.on('connection', function(socket){
-	console.log('A player connected');
-
-  players.push(socket);
-  socket.on('pass_turn',function(){
-     if(players[_turn] == socket){
-        resetTimeOut();
-        next_turn();
-     }
-  })
-
-  socket.on('disconnect', function(){
-    console.log('A player disconnected');
-    players.splice(players.indexOf(socket),1);
-    _turn--;
-    console.log("A number of players now ",players.length);
-  });
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
