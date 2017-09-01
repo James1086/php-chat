5fee868e2a33ec83cd9b6da93e5a8754c58d96e1
James1086 <james.castelow@gmail.com>
2017-09-01 21:29:55 +0100

    no message

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var active_player = '';
 var connected_players = [];
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
