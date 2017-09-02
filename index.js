ba2d7d4e99407262877db0ab4efcd5db4717b169
James1086 <james.castelow@gmail.com>
2017-09-02 21:55:28 +0100

    no message

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 var active_player = '';
 var connected_players = [];
+var path = require('path');
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
-	res.sendFile(__dirname + '/style.css');
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', function(socket){
 	connected_players.push(socket.id);
