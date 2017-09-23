4185f972037faf81481bf26b0933a663f818bc14
James1086 <james.castelow@gmail.com>
2017-09-23 21:00:56 +0100

    no message

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ server.listen(port, function () {
 });
 
 // Routing
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/',function(req,res){
+  res.sendFile((path.join(__dirname + '/index.html'));
+});
 
 
 /*
