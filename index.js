f6bd48c64175dda13ee2757100fcd23fc04e0f95
James1086 <james.castelow@gmail.com>
2017-09-23 20:40:08 +0100

    no message

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ var connected_players = {};
 
 app.use(express.static(__dirname + '/public'));
 
+/*
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
+*/
 
 app.get('/', function(req, res) {
 		res.sendFile(__dirname + '/public/index.html');
