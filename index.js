657c5f8f581667466a17f6b83d5ad22b3054997d
James1086 <james.castelow@gmail.com>
2017-09-23 20:52:57 +0100

    no message

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+// Setup basic express server
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app);
+var io = require('../..')(server);
 var port = process.env.PORT || 3000;
+
+
 var active_player = '';
 var connected_players = {};
 
-application.use("/public", express.static(path.join(__dirname, 'public')));
+
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+// Routing
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 /*
 app.get('/', function(req, res){
@@ -13,6 +25,7 @@ app.get('/', function(req, res){
 });
 */
 
+/*
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/public/index.html');
 });
@@ -43,3 +56,4 @@ io.on('connection', function(socket){
 http.listen(port, function(){
 	console.log('listening on *:' + port);
 });
+*/
