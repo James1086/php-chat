55901a4953584362adfaa6a7d05d301a673d3e7e
James1086 <james.castelow@gmail.com>
2017-10-08 17:12:26 +0100

    no message

--- index.js
+++ index.js
@@ -34,19 +34,17 @@ io.on('connection', function(socket){
 			var keys = Object.keys(connected_players);				
 			console.log(connected_players[keys[ keys.length * Math.random() << 0]]);
 			
-			/*
-			//Create the renderer
-			var renderer = PIXI.autoDetectRenderer(256, 256);
+				//Create the renderer
+	var renderer = PIXI.autoDetectRenderer(256, 256);
 
-			//Add the canvas to the HTML document
-			document.body.appendChild(renderer.view);
+	//Add the canvas to the HTML document
+	document.body.appendChild(renderer.view);
 
-			//Create a container object called the `stage`
-			var stage = new PIXI.Container();
+	//Create a container object called the `stage`
+	var stage = new PIXI.Container();
 
-			//Tell the `renderer` to `render` the `stage`
-			renderer.render(stage);
-			*/
+	//Tell the `renderer` to `render` the `stage`
+	renderer.render(stage);
 		}
 	});
 	
